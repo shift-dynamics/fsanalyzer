c647fe365663ea95594ac0d5e083613b23627110
Kristopher Wehage <kriswehage@gmail.com>
2021-01-24 11:38:33 -0800

    Use pathlib for system path import

--- test/test_draw_antisquat.py
+++ test/test_draw_antisquat.py
@@ -1,10 +1,13 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from pathlib import Path
+fsanalyzer_path = Path(__file__).parents[1].resolve()
+sys.path.append(os.fspath(fsanalyzer_path))
+
 from fsanalyzer.kinematics import FsAnalyzer
 
+
 if __name__ == "__main__":
-    file_path = os.path.join(os.path.dirname(__file__), "..", "data", "example.yaml")
+    file_path = Path(__file__).parents[1] / "data" / "example.yaml"
     fsanalyzer = FsAnalyzer(file_path)
     fsanalyzer.draw()
-

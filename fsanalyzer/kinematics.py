54631ef526c2c8814d0e1c37b2784103fd74756e
Kristopher Wehage <kriswehage@gmail.com>
2021-01-24 21:01:00 -0800

    Add config options to test program

--- fsanalyzer/kinematics.py
+++ fsanalyzer/kinematics.py
@@ -75,6 +75,7 @@ class FsAnalyzer:
         suspension = PrismaticJoint(
             suspension_lower.get_frame("distal"),
             suspension_upper.get_frame("origin"),
+            name="front_suspension",
             initial_position=0.0)
         self.joints.append(suspension)
         suspension_upper.update()
@@ -84,7 +85,7 @@ class FsAnalyzer:
         # draw frame front triangle
         # get position of bottom of steerer tube
         lower_steerer_tube = \
-            suspension_upper.get_frame("distal").absolute[:, 2][:2]
+            suspension_upper.get_frame("distal").get_position()
 
         # height of bottom bracket
         bb_height_absolute = \
@@ -94,7 +95,7 @@ class FsAnalyzer:
 
         top_of_head_tube_absolute = \
             bb_height_absolute + self.config["geometry"]["stack"]
-        y = top_of_head_tube_absolute - lower_steerer_tube[1, 0]
+        y = top_of_head_tube_absolute - lower_steerer_tube[1]
         x = -y * np.tan(
             np.radians(90.0 - self.config["geometry"]["head_tube_angle"]))
 
@@ -103,7 +104,7 @@ class FsAnalyzer:
         front_triangle.add_frame(
             Frame(name="bottom_bracket",
                   x=-self.config["geometry"]["reach"] + x,
-                  y=-lower_steerer_tube[1, 0] + bb_height_absolute))
+                  y=-lower_steerer_tube[1] + bb_height_absolute))
         head_tube_joint = RigidJoint(
             suspension_upper.get_frame("distal"),
             front_triangle.get_frame("origin"))
@@ -180,7 +181,44 @@ class FsAnalyzer:
             child = \
                 self.get_link(joint_["link_frame_pair"][1][0]).get_frame(
                     joint_["link_frame_pair"][1][1])
-            self.joints.append(RevoluteJoint(parent, child))
+            joint = RevoluteJoint(parent, child, name=joint_["name"])
+            joint.child.parent_body.update()
+            self.joints.append(joint)
+
+        seat_stay_distal_abs = \
+            self.get_link("rocker_link").get_frame(
+                "seat_stay_attachment").get_position()
+        seat_stay_link = self.get_link("seat_stay_link")
+        seat_stay_proximal_abs = seat_stay_link.get_frame(
+            "origin").get_position()
+        relative_pos = \
+            seat_stay_distal_abs - seat_stay_proximal_abs
+        rocker_link_attachment_frame = seat_stay_link.get_frame(
+            "rocker_link_attachment")
+        rocker_link_attachment_frame.relative[0, 2] = relative_pos[0]
+        rocker_link_attachment_frame.relative[1, 2] = relative_pos[1]
+        rocker_link_attachment_frame.update()
+
+        # add rear shock
+        shock_lower_pos = self.get_link("front_triangle").get_frame(
+            "shock_attachment").get_position()
+        shock_upper_pos = self.get_link("rocker_link").get_frame(
+            "shock_attachment").get_position()
+        print(
+            "shock distance: ",
+            np.linalg.norm(shock_lower_pos - shock_upper_pos))
+
+        shock_lower = Link("shock_lower")
+        shock_lower.add_frame(
+            Frame(name="distal",
+                  x=0.0,
+                  y=self.config["shock"]["eye_to_eye"] / 2.0))
+
+        shock_upper = Link("shock_upper")
+        shock_upper.add_frame(
+            Frame(name="distal",
+                  x=0.0,
+                  y=self.config["shock"]["eye_to_eye"] / 2.0))
 
         # add chainrings
         for i, ring in \
@@ -199,10 +237,10 @@ class FsAnalyzer:
         fixed_link.add_frame(
             Frame(name="rear_wheel",
                   x=-x + front_triangle.get_frame(
-                      "bottom_bracket").absolute[0, 2],
+                      "bottom_bracket").get_position()[0],
                   y=front_wheel_radius))
         fixed_link.update()
-        print(front_triangle.get_frame("bottom_bracket").absolute[0, 2])
+        print(front_triangle.get_frame("bottom_bracket").get_position()[0])
 
         rear_wheel = Wheel("rear_wheel", front_wheel_radius)
         rear_wheel_attachment = RigidJoint(
@@ -228,27 +266,55 @@ class FsAnalyzer:
         else:
             raise Exception(f"Could not find link {name}")
 
+    def get_joint(self, name):
+        for joint in self.joints:
+            if joint.name == name:
+                return joint
+        else:
+            raise Exception(f"Could not find joint {name}")
+
     def draw_chainlines(self, ax, ax2=None):
         front_triangle = self.get_link("front_triangle")
-        rear_wheel = self.get_link("rear_wheel")
+        p1 = self.get_link("front_triangle").get_frame(
+            "bottom_bracket").get_position()
+        p2 = self.get_link("rear_wheel").get_frame(
+            "origin").get_position()
 
         r1 = geometry.chain_ring_radius(
             self.config["drive_train"]["front_chainrings"][0])
-        p1 = front_triangle.get_frame("bottom_bracket").absolute[:2, 2]
-        p2 = rear_wheel.get_frame("origin").absolute[:2, 2]
 
+        ic = None
+        attachment_link = self.config["wheels"]["rear"]["attachment_link"]
+        if attachment_link == "seat_stay_link":
+            l1 = [self.get_link("chain_stay_link").get_frame(
+                  "seat_stay_attachment").get_position(),
+                  self.get_link("chain_stay_link").get_frame(
+                  "origin").get_position()]
+            l2 = [self.get_link("rocker_link").get_frame(
+                  "seat_stay_attachment").get_position(),
+                  self.get_link("rocker_link").get_frame(
+                  "origin").get_position()]
+            ic = geometry.line_intersection(l1, l2)
+        elif attachment_link == "chain_stay_link":
+            ic = self.get_link("front_triangle").get_frame(
+                "chain_stay_attachment").get_position()
+        else:
+            raise Exception(
+                "invalid setting found for wheel `attachment_link`, "
+                f"{attachment_link} was specified, but only `chain_stay_link`"
+                "and `seat_stay_link` are valid values")
         # draw instant center (@TODO get this from suspension geometry, just
         # adding a placeholder for now)
-        ic = [p1[0, 0] + 120.0, p1[1, 0] + 73.0]
         ax.plot([ic[0], ic[0] + 50.0], [ic[1], ic[1]], color="red")
         ax.plot([ic[0], ic[0]], [ic[1], ic[1] + 50], color="blue")
-        ax.plot([p2[0, 0], ic[0]], [p2[1, 0], ic[1]], color="black",
+
+        ax.plot([p2[0], ic[0]], [p2[1], ic[1]], color="black",
                 linestyle="--")
         ax.text(ic[0], ic[1], "instant_center")
 
         d1 = p2 - p1
         d1_norm = np.linalg.norm(d1)
-        theta1 = np.pi - np.arctan2(d1[1, 0], d1[0, 0])
+        theta1 = np.pi - np.arctan2(d1[1], d1[0])
 
         com = self.config["center_of_mass"]["height"]
         antisquat = []
@@ -258,23 +324,23 @@ class FsAnalyzer:
             theta2 = np.arccos((r2 - r1) / d1_norm)
             theta3 = theta1 - theta2
 
-            c1 = [p1[0, 0] + r1 * np.cos(theta3),
-                  p1[1, 0] - r1 * np.sin(theta3)]
+            c1 = [p1[0] + r1 * np.cos(theta3),
+                  p1[1] - r1 * np.sin(theta3)]
 
-            c2 = [p2[0, 0] + r2 * np.cos(theta3),
-                  p2[1, 0] - r2 * np.sin(theta3)]
+            c2 = [p2[0] + r2 * np.cos(theta3),
+                  p2[1] - r2 * np.sin(theta3)]
 
             ax.plot([c1[0], c2[0]], [c1[1], c2[1]])
 
             # find intersection between chainline and line drawn from rear
             # hub to instant center
             intercept = geometry.line_intersection(
-                [c1, c2], [[p2[0, 0], p2[1, 0]], ic])
+                [c1, c2], [[p2[0], p2[1]], ic])
             ax.plot(intercept[0], intercept[1], color="red", marker="o")
 
             antisquat_intercept = geometry.line_intersection(
-                [[p2[0, 0], 0.0], intercept], [[0.0, 0.0], [0.0, 100.0]])
-            ax.plot([p2[0, 0], antisquat_intercept[0]],
+                [[p2[0], 0.0], intercept], [[0.0, 0.0], [0.0, 100.0]])
+            ax.plot([p2[0], antisquat_intercept[0]],
                     [0.0, antisquat_intercept[1]], color="red")
             antisquat_pct = antisquat_intercept[1] / com * 100.0
             antisquat.append(antisquat_pct)
@@ -282,17 +348,30 @@ class FsAnalyzer:
         if ax2 is not None:
             ax2.scatter([0] * len(antisquat), antisquat)
 
-        bb_x = front_triangle.get_frame("bottom_bracket").absolute[0, 2]
+        bb_x = front_triangle.get_frame("bottom_bracket").get_position()[0]
         ax.plot([0.0, 0.0], [0.0, com + 50.0],
                 color="black", linestyle="--")
         ax.plot([bb_x, 50.0], [com, com], color="black", linestyle="--")
         ax.text(50.0, com, "100% antisquat", verticalalignment="center")
 
-    def draw(self):
+    def draw(self, draw_frames=True, draw_labels=True):
         fig, ax = plt.subplots(2, 1)
         ax[0].set_aspect(1)
-        ax[0].set_ylim(0.0, 1200.0)
-        ax[0].set_xlim(-2000.0, 500.0)
+
+        upper_lim = \
+            ceil(self.config["center_of_mass"]["height"] /  # noqa
+                 100) * 100 + 100
+        ax[0].set_ylim(0.0, upper_lim)
+
+        wheel_radius = self.config["wheels"]["front"]["diameter"] / 2.0
+        right_lim = \
+            ceil((self.get_link("front_wheel").get_frame(  # noqa
+                 "origin").get_position()[0] + wheel_radius) / 100) * 100 + 100
+        left_lim = \
+            floor((self.get_link("rear_wheel").get_frame(  # noqa
+                  "origin").get_position()[0] - wheel_radius) / 100) * \
+            100 - 100
+        ax[0].set_xlim(left_lim, right_lim)
 
         travel = 120.0
         sag = 0.3
@@ -309,7 +388,8 @@ class FsAnalyzer:
         for link in self.links:
             if link.name != "fixed_link":
                 link.draw(ax[0])
-            for frame in link.frames:
-                frame.draw(ax[0], scale=50)
+            if draw_frames:
+                for frame in link.frames:
+                    frame.draw(ax[0], scale=50, draw_labels=draw_labels)
         self.draw_chainlines(ax[0], ax[1])
         plt.show()
--- test/test_draw_antisquat.py
+++ test/test_draw_antisquat.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 from pathlib import Path
 fsanalyzer_path = Path(__file__).parents[1].resolve()
 sys.path.append(os.fspath(fsanalyzer_path))
@@ -7,7 +8,20 @@ sys.path.append(os.fspath(fsanalyzer_path))
 from fsanalyzer.kinematics import FsAnalyzer
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--no-frames', help='disable drawing coordinate frames',
+        action='store_true')
+    parser.add_argument(
+        '--no-labels', help='disable drawing labels',
+        action='store_true')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args()
     file_path = Path(__file__).parents[1] / "data" / "example.yaml"
     fsanalyzer = FsAnalyzer(file_path)
-    fsanalyzer.draw()
+    fsanalyzer.draw(
+        draw_frames=not args.no_frames, draw_labels=not args.no_labels)
